407e6d633fb51ee5a5e61546dfb6904b5a9ed085
Subhashchandra Saradhara <thedestinysolutions@gmail.com>
2023-07-03 19:10:22 +0530

    Assets page design

--- src/Components/Assests/Assets.jsx
+++ src/Components/Assests/Assets.jsx
@@ -4,7 +4,34 @@ import Navbar from "../Navbar";
 import { TiFolderOpen } from 'react-icons/ti'
 import { AiOutlineCloudUpload, AiOutlineUnorderedList } from 'react-icons/ai'
 import { RxDashboard } from 'react-icons/rx'
+import AssetsAPI from './Assestsapi'
+import { BsThreeDots } from 'react-icons/bs'
+import '../../Styles/assest.css'
+import { FiUpload } from 'react-icons/fi'
+import { MdPlaylistPlay } from 'react-icons/md'
+import { FiDownload } from 'react-icons/fi'
+import { RiDeleteBin5Line } from 'react-icons/ri'
+import { CgMoveRight } from 'react-icons/cg'
 const Assets = ({ sidebarOpen, setSidebarOpen }) => {
+    const [tabitems, setTabitems] = useState(AssetsAPI)
+    const [activetab, setActivetab] = useState(1)
+    const filterItems = (catgitem) => {
+        const updatedItem = AssetsAPI.filter((curEle) => {
+            return curEle.category == catgitem;
+        })
+        setTabitems(updatedItem);
+
+    }
+
+    const handleActiveBtnClick = (btnNumber) => {
+        setActivetab(btnNumber)
+    }
+    const [assetsdw, setassetsdw] = useState(false)
+    const [asstab, setTogglebtn] = useState(1);
+    function updatetoggle(id) {
+        setTogglebtn(id);
+
+    }
     return (
         <>
             <div className="flex border-b border-gray py-3">
@@ -28,8 +55,8 @@ const Assets = ({ sidebarOpen, setSidebarOpen }) => {
                             </button>
 
                             <ul className="flex items-center  mr-3  rounded-full  border border-primary">
-                                <li className="flex items-center"><button className="px-2"><RxDashboard className="text-primary text-lg" /></button></li>
-                                <li className="flex items-center"><button className="px-2"><AiOutlineUnorderedList /></button></li>
+                                <li className="flex items-center "><button className={asstab === 1 ? 'tabshow tabassactive px-5' : 'asstab px-5'} onClick={() => updatetoggle(1)}><RxDashboard className="text-primary text-lg" /></button></li>
+                                <li className="flex items-center "><button className={asstab === 2 ? 'tabshow tabassactive pr-5' : 'asstab pr-5'} onClick={() => updatetoggle(2)}><AiOutlineUnorderedList /></button></li>
 
                             </ul>
                             <button>
@@ -37,6 +64,67 @@ const Assets = ({ sidebarOpen, setSidebarOpen }) => {
                             </button>
                         </div>
                     </div>
+
+                    <div className="tabs mt-5">
+
+                        <button className={activetab === 1 ? 'tabactivebtn ' : 'tabbtn'} onClick={() => { setTabitems(AssetsAPI); handleActiveBtnClick(1) }}>All</button>
+                        <button className={activetab === 2 ? 'tabactivebtn ' : 'tabbtn'} onClick={() => { filterItems('image'); handleActiveBtnClick(2) }}>Images</button>
+                        <button className={activetab === 3 ? 'tabactivebtn ' : 'tabbtn'} onClick={() => { filterItems('video'); handleActiveBtnClick(3) }}>Video</button>
+                        <button className={activetab === 4 ? 'tabactivebtn ' : 'tabbtn'} onClick={() => { filterItems('doc'); handleActiveBtnClick(4) }}>Doc</button>
+                        <button className={activetab === 5 ? 'tabactivebtn ' : 'tabbtn'} onClick={() => { filterItems('apps'); handleActiveBtnClick(5) }}>Apps</button>
+                    </div>
+
+                    <div className={asstab === 1 ? 'show-togglecontent active w-full tab-details mt-10' : 'togglecontent'}>
+                        <div className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-12 gap-4">
+
+                            {
+                                tabitems.map((elem) => {
+                                    const { id, Image, icon, title, subtitle } = elem;
+                                    return (
+                                        <>
+
+                                            <div className="relative">
+                                                <div className="imagebox relative">
+                                                    <img src={Image} className="w-full rounded-2xl" />
+                                                    <div className="checkbox flex justify-between absolute top-5 px-4 w-full">
+                                                        <input type="checkbox" className="w-[20px] h-[20px]" />
+                                                        <button onClick={() => setassetsdw((prev) => !prev)}><BsThreeDots className="text-2xl" /></button>
+                                                        {
+                                                            assetsdw &&
+
+                                                            <div className="assetsdw">
+                                                                <ul>
+                                                                    <li className="flex text-sm items-center"><FiUpload className="mr-2 text-lg" />Set to Screen</li>
+                                                                    <li className="flex text-sm items-center" ><MdPlaylistPlay className="mr-2 text-lg" />Add to Playlist</li>
+                                                                    <li className="flex text-sm items-center" ><FiDownload className="mr-2 text-lg" />Download</li>
+                                                                    <li className="flex text-sm items-center" ><CgMoveRight className="mr-2 text-lg" />Move to</li>
+                                                                    <li className="flex text-sm items-center" ><RiDeleteBin5Line className="mr-2 text-lg" />Move to Trast</li>
+                                                                </ul>
+                                                            </div>
+
+                                                        }
+
+                                                    </div>
+                                                </div>
+                                                <div className="tabicon text-center absolute left-2/4">
+                                                    <p>{icon}</p>
+
+                                                </div>
+                                                <div className="title text-center relative mt-10">
+                                                    <h2>{title}</h2>
+                                                    <h2>{subtitle}</h2>
+                                                </div>
+                                            </div>
+
+                                        </>
+                                    )
+
+                                })
+                            }
+                        </div>
+                    </div>
+
+
                 </div>
             </div>
             }
