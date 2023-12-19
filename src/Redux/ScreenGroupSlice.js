5c2b76d0d4eac4cfbee9efb0ba2443cafe53d507
dhruv <dhruv.chavda@thedestinysolutions.com>
2023-12-18 19:35:06 -0800

    Screen Group model , forgotpassword , logini ,firebase remove

--- src/Redux/ScreenGroupSlice.js
+++ src/Redux/ScreenGroupSlice.js
@@ -1,6 +1,5 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
-import { getUrl } from "../Pages/Api";
 
 const initialState = {
   data: [],
@@ -8,14 +7,29 @@ const initialState = {
   error: null,
 };
 
-// getScreenGroup 
+// SelectByUserScreen
+export const SelectByUserScreen = createAsyncThunk(
+  "data/SelectByUserScreen",
+  async ({ config }, { rejectWithValue }) => {
+    try {
+      const data = await axios.request(config);
+      return data.data;
+    } catch (error) {
+      rejectWithValue(error?.response?.data?.message);
+    }
+  }
+);
+
+// getScreenGroup
 export const getScreenGroup = async (payload) => {
- const queryParams = new URLSearchParams(payload).toString();
- const response = await axios.post(`your_api_endpoint?${queryParams}`);
+  const queryParams = new URLSearchParams(payload).toString();
+  const response = await axios.post(`your_api_endpoint?${queryParams}`);
   return response.data;
 };
 
-export const getGroupData = createAsyncThunk("data/fetchApiData",async (payload) => {
+export const getGroupData = createAsyncThunk(
+  "data/fetchApiData",
+  async (payload) => {
     try {
       const data = await getScreenGroup(payload);
       return data;
@@ -25,28 +39,29 @@ export const getGroupData = createAsyncThunk("data/fetchApiData",async (payload)
   }
 );
 
-
 // Save ScreenGroup
 export const saveScreenGroup = async (payload) => {
-    const response = await axios.post('your_api_endpoint', payload);
-    // Check conditions in the response and return accordingly
-    if (response.data.status) {
-        return { status: true, message: response.data.message, data: response.data.data };
-    } else {
-        return { status: false, message: 'Failed to save data' };
-    }
+  const response = await axios.post("your_api_endpoint", payload);
+  // Check conditions in the response and return accordingly
+  if (response.data.status) {
+    return {
+      status: true,
+      message: response.data.message,
+      data: response.data.data,
+    };
+  } else {
+    return { status: false, message: "Failed to save data" };
+  }
 };
-   
-export const saveGroupData = createAsyncThunk("data/save", async (payload) => {
-    try {
-        const data = await saveScreenGroup(payload);
-        return data;
-    } catch (error) {
-        throw error;
-    }
-}
-);
 
+export const saveGroupData = createAsyncThunk("data/save", async (payload) => {
+  try {
+    const data = await saveScreenGroup(payload);
+    return data;
+  } catch (error) {
+    throw error;
+  }
+});
 
 const screenGroupSlice = createSlice({
   name: "screenGroup",
@@ -54,31 +69,50 @@ const screenGroupSlice = createSlice({
   reducers: {},
   extraReducers: (builder) => {
     builder
-      .addCase(getGroupData.pending, (state) => {  // getScreenGroup
+
+      .addCase(SelectByUserScreen.pending, (state) => {   // SelectByUserScreen
         state.status = "loading";
       })
-      .addCase(getGroupData.fulfilled, (state, action) => {   // getScreenGroup
+      .addCase(SelectByUserScreen.fulfilled, (state, action) => {    // SelectByUserScreen
+        // console.log(action.payload.data,"---------");
         state.status = "succeeded";
-        state.data = action.payload;
+        state.data = action.payload.data;
       })
-      .addCase(getGroupData.rejected, (state, action) => {     // getScreenGroup
+      .addCase(SelectByUserScreen.rejected, (state, action) => {  // SelectByUserScreen
         state.status = "failed";
         state.error = action.error.message;
       })
 
-      .addCase(saveGroupData.pending, (state) => {   // Save ScreenGroup
-        state.status = 'loading';
+      .addCase(getGroupData.pending, (state) => {
+        // getScreenGroup
+        state.status = "loading";
       })
-      .addCase(saveGroupData.fulfilled, (state, action) => {   // Save ScreenGroup
-        state.status = 'succeeded';
+      .addCase(getGroupData.fulfilled, (state, action) => {
+        // getScreenGroup
+        state.status = "succeeded";
+        state.data = action.payload;
+      })
+      .addCase(getGroupData.rejected, (state, action) => {
+        // getScreenGroup
+        state.status = "failed";
+        state.error = action.error.message;
+      })
+
+      .addCase(saveGroupData.pending, (state) => {
+        // Save ScreenGroup
+        state.status = "loading";
+      })
+      .addCase(saveGroupData.fulfilled, (state, action) => {
+        // Save ScreenGroup
+        state.status = "succeeded";
         state.message = action.payload.message;
         state.data = action.payload.data;
       })
-      .addCase(saveGroupData.rejected, (state, action) => {    // Save ScreenGroup
-        state.status = 'failed';
-        state.message = action.error.message || 'Failed to save data';
+      .addCase(saveGroupData.rejected, (state, action) => {
+        // Save ScreenGroup
+        state.status = "failed";
+        state.message = action.error.message || "Failed to save data";
       });
-      
   },
 });
 
