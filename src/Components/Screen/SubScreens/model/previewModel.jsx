632fe04f4f39980c534a9b0d78a8d848fb269fcf
Harsh <harsh.mehta@thedestinysolutions.com>
2024-01-25 17:16:11 +0530

    Solve Design Issue

--- src/Components/Screen/SubScreens/model/previewModel.jsx
+++ src/Components/Screen/SubScreens/model/previewModel.jsx
@@ -1,69 +1,67 @@
 import React from "react";
-import Modal from 'react-responsive-modal';
+import Modal from "react-responsive-modal";
 import "react-responsive-modal/styles.css";
-import ReactPlayer from 'react-player'
+import ReactPlayer from "react-player";
 
 // Define a custom style with a fixed height
 const modalStyle = {
-    width: '80%', // You can adjust the width based on your preference
-    height: '500px', // Set the fixed height here
+  width: "80%", // You can adjust the width based on your preference
+  height: "70vh", // Set the fixed height here
 };
 
-const Preview = ({
-    open,
-    onClose,
-    previewData
-}) => {
+const Preview = ({ open, onClose, previewData }) => {
+  const handleModalClose = () => {
+    onClose();
+  };
 
-    const handleModalClose = () => {
-        onClose();
-    };
+  console.log("previewData", previewData);
 
-    console.log("previewData", previewData);
-
-    return (
-        <>
-            <Modal
-                open={open}
-                onClose={handleModalClose}
-                center
-                styles={{ modal: modalStyle }}
+  return (
+    <>
+      <Modal
+        open={open}
+        onClose={handleModalClose}
+        center
+        styles={{ modal: modalStyle }}
+      >
+        <div className="w-full text-center bg-white rounded-2xl  h-full">
+          {previewData.assetType === "Video" && (
+            <video
+              controls
+              className="flex justify-center relative w-full h-full"
+              style={{ maxWidth: "100%", height: "100%" }}
             >
-                <hr className="m-2 h-10" />
-                <div className="w-full p-4 text-center bg-white rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700 h-[300px]">
+              <source src={previewData.assetURL} type="video/mp4" />
+              Your browser does not support the video tag.
+            </video>
+          )}
 
-                    {previewData.assetType === 'Video' &&
+          {previewData.assetType === "OnlineVideo" && (
+            <video
+              controls
+              className="flex justify-center relative w-full h-full"
+              style={{ maxWidth: "100%", height: "100%" }}
+            >
+              <source src={previewData.assetURL} type="video/mp4" />
+              Your browser does not support the video tag.
+            </video>
+          )}
 
-                        <video controls className="w-full h-auto" style={{ maxWidth: "100%", height: "auto" }}>
-                            <source src={previewData.assetURL} type="video/mp4" />
-                            Your browser does not support the video tag.
-                        </video>
-                    }
+          {previewData.assetType === "OnlineImage" && (
+            <div className="flex justify-center relative w-full h-full">
+              <img src={previewData.assetURL} className="h-full rounded-lg" />
+            </div>
+          )}
 
-                    {previewData.assetType === 'OnlineVideo' &&
-                        <video controls className="w-full h-auto" style={{ maxWidth: "100%", height: "auto" }}>
-                            <source src={previewData.assetURL} type="video/mp4" />
-                            Your browser does not support the video tag.
-                        </video>
-                    }
-
-
-                    {previewData.assetType === 'OnlineImage' &&
-                        <div className="">
-                            <img src={previewData.assetURL} className="h-48 w-100 rounded-lg" />
-                        </div>
-                    }
-
-                    {previewData.assetType === 'Image' &&
-                        <div className="">
-                            <img src={previewData.assetURL} className="h-48 w-100 rounded-lg" />
-                        </div>
-                    }
-
-                </div>
-            </Modal>
-        </>
-    );
-}
+          {previewData.assetType === "Image" && (
+            <div className="flex justify-center relative w-full h-full">
+              <img src={previewData.assetURL} className="h-full rounded-lg" />
+            </div>
+          )}
+        </div>
+      </Modal>
+    </>
+  );
+};
 
 export default Preview;
