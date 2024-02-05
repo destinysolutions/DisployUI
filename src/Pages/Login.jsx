1fe168d23e81a330c45195d3274a4821026961d0
unknown <hetal.prajapati@thedestinysolutions.com>
2024-02-05 16:39:41 +0530

    popup changes

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -46,7 +46,7 @@ const Login = () => {
   // const { loginUser } = useUser();
   //using show or hide password field
   const dispatch = useDispatch();
-  
+
   const [showPassword, setShowPassword] = useState(false);
   const modalRef = useRef(null);
   const [showModal, setShowModal] = useState(false);
@@ -76,7 +76,6 @@ const Login = () => {
     captcha: Yup.string().required("captcha is required."),
   });
 
-
   const formik = useFormik({
     initialValues: {
       password: "",
@@ -86,16 +85,20 @@ const Login = () => {
     },
     validationSchema: validationSchema,
     onSubmit: (values) => {
-
       if (!isCheckboxChecked) {
         toast.error("Please check & accept the terms and conditions.");
         return; // Exit the submission process if checkbox is not checked
       }
-      
+
       let data = JSON.stringify({
         emailID: values.emailID,
         password: values.password,
-        SystemTimeZone:new Date().toLocaleDateString(undefined, {day:'2-digit',timeZoneName: 'long' }).substring(4)
+        SystemTimeZone: new Date()
+          .toLocaleDateString(undefined, {
+            day: "2-digit",
+            timeZoneName: "long",
+          })
+          .substring(4),
       });
 
       let config = {
@@ -311,9 +314,7 @@ const Login = () => {
 
   const handleAcceptTerms = () => {
     setShowModal(false);
-  }
-
-
+  };
 
   return (
     <>
@@ -528,15 +529,35 @@ const Login = () => {
 
       {showModal && (
         <div className="backdrop bg-white">
-          <div ref={modalRef} className="user-model">
+          <div ref={modalRef} className="user-model-TC">
             <div className="relative  overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
               <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600 p-4">
                 <h3 className="text-xl font-semibold text-gray-900 dark:text-white w-full max-w-2xl max-h-full">
                   Terms And Conditions
                 </h3>
-                <button type="button" className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="default-modal" onClick={() => { setShowModal(false); setIsCheckboxChecked() }}>
-                  <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+                <button
+                  type="button"
+                  className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                  data-modal-hide="default-modal"
+                  onClick={() => {
+                    setShowModal(false);
+                    setIsCheckboxChecked();
+                  }}
+                >
+                  <svg
+                    className="w-3 h-3"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 14 14"
+                  >
+                    <path
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+                    />
                   </svg>
                   <span className="sr-only">Close modal</span>
                 </button>
@@ -546,32 +567,77 @@ const Login = () => {
                   <li>
                     <b>Prohibited Activities</b>
                     <ul className="ps-5 mt-2 space-y-1 list-disc list-inside">
-                      <li>You may not access or use the Site for any purpose other than that for which we make the Site available. The Site may not be used in connection with any commercial endeavors except those that are specifically endorsed or approved by us.</li>
+                      <li>
+                        You may not access or use the Site for any purpose other
+                        than that for which we make the Site available. The Site
+                        may not be used in connection with any commercial
+                        endeavors except those that are specifically endorsed or
+                        approved by us.
+                      </li>
                     </ul>
                   </li>
                   <li>
                     <b>Contribution License</b>
                     <ul className="ps-5 mt-2 space-y-1 list-disc list-inside">
-                      <li>You and the Site agree that we may access, store, process, and use any information and personal data that you provide following the terms of the Privacy Policy and your choices (including settings). By submitting suggestions or other feedback regarding the Site, you agree that we can use and share such feedback for any purpose without compensation to you.</li>
+                      <li>
+                        You and the Site agree that we may access, store,
+                        process, and use any information and personal data that
+                        you provide following the terms of the Privacy Policy
+                        and your choices (including settings). By submitting
+                        suggestions or other feedback regarding the Site, you
+                        agree that we can use and share such feedback for any
+                        purpose without compensation to you.
+                      </li>
                     </ul>
                   </li>
                   <li>
                     <b>Term And Termination</b>
                     <ul className="ps-5 mt-2 space-y-1 list-disc list-inside">
-                      <li>These terms of use shall remain in full force and effect while you use the site. Without limiting any other provision of these terms of use, we reserve the right to, in our sole discretion and without notice or liability, deny access to and use of the site and the marketplace offerings (including blocking certain ip addresses), to any person for any reason or for no reason, including without limitation for breach of any representation, warranty, or covenant contained in these terms of use or of any applicable law or regulation. We may terminate your use or participation in the site and the marketplace offerings or delete any content or information that you posted at any time, without warning, in our sole discretion.</li>
+                      <li>
+                        These terms of use shall remain in full force and effect
+                        while you use the site. Without limiting any other
+                        provision of these terms of use, we reserve the right
+                        to, in our sole discretion and without notice or
+                        liability, deny access to and use of the site and the
+                        marketplace offerings (including blocking certain ip
+                        addresses), to any person for any reason or for no
+                        reason, including without limitation for breach of any
+                        representation, warranty, or covenant contained in these
+                        terms of use or of any applicable law or regulation. We
+                        may terminate your use or participation in the site and
+                        the marketplace offerings or delete any content or
+                        information that you posted at any time, without
+                        warning, in our sole discretion.
+                      </li>
                     </ul>
                   </li>
                 </ol>
               </div>
               <div className="flex items-center p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600">
-                <button data-modal-hide="default-modal" type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" onClick={() => handleAcceptTerms()}>I accept</button>
-                <button data-modal-hide="default-modal" type="button" className="ms-3 text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600" onClick={() => { setShowModal(false); setIsCheckboxChecked() }}>Decline</button>
+                <button
+                  data-modal-hide="default-modal"
+                  type="button"
+                  className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                  onClick={() => handleAcceptTerms()}
+                >
+                  I accept
+                </button>
+                <button
+                  data-modal-hide="default-modal"
+                  type="button"
+                  className="ms-3 text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600"
+                  onClick={() => {
+                    setShowModal(false);
+                    setIsCheckboxChecked();
+                  }}
+                >
+                  Decline
+                </button>
               </div>
             </div>
           </div>
         </div>
       )}
-
     </>
   );
 };
