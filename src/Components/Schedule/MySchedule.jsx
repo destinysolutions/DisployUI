716e652409d354fc19cb4a8ad8656441eb348a5e
dhruv <dhruv.chavda@thedestinysolutions.com>
2024-03-02 04:06:43 +0530

    Solve pagination Bug

--- src/Components/Schedule/MySchedule.jsx
+++ src/Components/Schedule/MySchedule.jsx
@@ -1040,7 +1040,7 @@ const MySchedule = ({ sidebarOpen, setSidebarOpen }) => {
                       {/* <span>{`Page ${currentPage} of ${totalPages}`}</span> */}
                       <button
                         onClick={() => handlePageChange(currentPage + 1)}
-                        disabled={currentPage === totalPages}
+                        disabled={(currentPage === totalPages) || (schedules?.length === 0)}
                         className="flex hover:bg-white hover:text-primary cursor-pointer items-center justify-center px-3 h-8 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
                       >
                         {sidebarOpen ? "Next" : ""}
--- src/Components/Screen/Screens.jsx
+++ src/Components/Screen/Screens.jsx
@@ -1682,7 +1682,7 @@ const Screens = ({ sidebarOpen, setSidebarOpen }) => {
                       {/* <span>{`Page ${currentPage} of ${totalPages}`}</span> */}
                       <button
                         onClick={() => handlePageChange(currentPage + 1)}
-                        disabled={currentPage === totalPages}
+                        disabled={(currentPage === totalPages) || (screens?.length === 0)}
                         className="flex hover:bg-white hover:text-primary cursor-pointer items-center justify-center px-3 h-8 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 hover:text-gray-700 "
                       >
                         {sidebarOpen ? "Next" : ""}
