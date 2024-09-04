a01e045457e0fbb5281e1339c6acc5e2033cdfe7
Destiny Solution <thedestinysolutions@gmail.com>
2024-09-04 09:59:43 +0530

    create book your slot page

--- src/Components/Screen/SubScreens/BookSlot/ImageUploadPopup.jsx
+++ src/Components/Screen/SubScreens/BookSlot/ImageUploadPopup.jsx
@@ -1,14 +1,40 @@
 import { Button } from '@material-tailwind/react';
 import React, { useState } from 'react'
 import { AiOutlineCloseCircle } from 'react-icons/ai'
+import { TbCloudUpload } from 'react-icons/tb';
+
 
 const ImageUploadPopup = ({ index, isOpen, onClose, onSubmit }) => {
+
+    const [ImageType, setImageType] = useState('Horizontal');
+
+    const [progress, setProgress] = useState(0);
 
     const [verticalImage, setVerticalImage] = useState(null);
     const [horizontalImage, setHorizontalImage] = useState(null);
 
     const [verticalFileName, setVerticalFileName] = useState('');
     const [horizontalFileName, setHorizontalFileName] = useState('');
+
+
+
+    const handleDragOver = (e) => {
+        e.preventDefault();
+        e.stopPropagation();
+    };
+
+    const handleDrop = (e) => {
+        e.preventDefault();
+        e.stopPropagation();
+        const files = e.dataTransfer.files[0];
+        handleFileUpload(files);
+
+    };
+    const handleFileInputChange = (e) => {
+        const files = e.target.files[0];
+        handleFileUpload(files);
+    };
+
 
     const handleVerticalFileUpload = (e) => {
         const file = e.target.files[0];
@@ -22,13 +48,31 @@ const ImageUploadPopup = ({ index, isOpen, onClose, onSubmit }) => {
         }
     };
 
-    const handleHorizontalFileUpload = (e) => {
-        const file = e.target.files[0];
+    const handleFileUpload = (file) => {
+
+        // const file = e.target.files[0];
         if (file) {
             const reader = new FileReader();
+
+            reader.onloadstart = () => {
+                setProgress(0); // Reset progress when upload starts
+            };
+
+            reader.onprogress = (event) => {
+                if (event.loaded && event.total) {
+                    const percentCompleted = Math.round((event.loaded / event.total) * 100);
+                    setProgress(percentCompleted);
+                }
+            };
+           
             reader.onloadend = () => {
-                setHorizontalImage(file);
-                setHorizontalFileName(file.name);
+                if (ImageType === 'Horizontal') {
+                    setHorizontalImage(file);
+                    setHorizontalFileName(file.name);
+                } else {
+                    setVerticalImage(file);
+                    setVerticalFileName(file.name);
+                }
             };
             reader.readAsDataURL(file);
         }
@@ -64,7 +108,7 @@ const ImageUploadPopup = ({ index, isOpen, onClose, onSubmit }) => {
                                     <div className="schedual-table bg-white rounded-xl mt-5 px-3">
                                         <div className="relative overflow-x-auto sc-scrollbar rounded-lg">
 
-                                            <div class="grid grid-cols-1 gap-6 sm:grid-cols-2 m-auto">
+                                            {/* <div class="grid grid-cols-1 gap-6 sm:grid-cols-2 m-auto">
                                                 <div class="mb-3 text-gray-500 dark:text-gray-400 m-auto">
                                                     <figure className="max-w-lg">
                                                         {verticalFileName && (
@@ -109,6 +153,79 @@ const ImageUploadPopup = ({ index, isOpen, onClose, onSubmit }) => {
                                                             Horizontal File Upload
                                                         </label>
                                                     </figure>
+                                                </div>
+                                            </div> */}
+                                            <div class=" m-auto">
+                                                <div className="flex gap-6 justify-center m-auto">
+                                                    <div className=" flex items-center">
+                                                        <input
+                                                            type="radio"
+                                                            value={ImageType}
+                                                            checked={ImageType === "Horizontal"}
+                                                            name="Cel"
+                                                            id='Horizontal'
+                                                            onChange={() => setImageType("Horizontal")}
+                                                        />
+                                                        <label for='Horizontal' className="ml-2 lg:text-base md:text-base sm:text-xs xs:text-xs">
+                                                            {horizontalFileName ? horizontalFileName : 'Horizontal'}
+                                                            {/* Horizontal */}
+                                                        </label>
+                                                    </div>
+                                                    <div className=" flex items-center">
+                                                        <input
+                                                            id='Vertical'
+                                                            type="radio"
+                                                            value={ImageType}
+                                                            checked={ImageType === "Vertical"}
+                                                            name="Cel"
+                                                            onChange={() => setImageType("Vertical")}
+                                                        />
+                                                        <label for='Vertical' className="ml-2 lg:text-base md:text-base sm:text-xs xs:text-xs">
+                                                            {verticalFileName ? verticalFileName : 'Vertical'}
+                                                            {/* Vertical */}
+                                                        </label>
+                                                    </div>
+                                                </div>
+
+                                                <div
+                                                    className="flex justify-center items-center h-36 rounded-lg"
+                                                    onDragOver={handleDragOver}
+                                                    onDrop={handleDrop}
+                                                >
+                                                    <label htmlFor="fileInput" className="cursor-pointer flex flex-col items-center justify-center">
+                                                        <TbCloudUpload size={35} />
+                                                        <h3 className=" text-black text-sm px-5 p-2 font-semibold">Select Files to upload</h3>
+                                                        <h4 className='text-xs font-medium'>Drag and Drop  or Upload your content</h4>
+                                                        <input
+                                                            type="file"
+                                                            id="fileInput"
+                                                            className="hidden"
+                                                            onChange={handleFileInputChange}
+                                                            multiple
+                                                            accept="image/* , video/*"
+                                                        />
+                                                    </label>
+                                                </div>
+                                                <div>
+                                                    {progress > 0 && (
+                                                        <div className="flex items-center gap-x-3 whitespace-nowrap" key={index}>
+                                                            <div className="flex w-full h-2 bg-gray-200 rounded-full overflow-hidden">
+                                                                <div
+                                                                    className="flex flex-col justify-center rounded-full overflow-hidden text-xs text-white text-center whitespace-nowrap transition-all duration-500"
+                                                                    style={{
+                                                                        width: `${progress}%`,
+                                                                        backgroundColor: progress < 100 ? '#3182ce' : '#38a169'
+                                                                    }}
+                                                                ></div>
+                                                            </div>
+                                                            <div className="w-10 text-end">
+                                                                <span className="text-sm text-gray-800">
+                                                                    {`${progress}% `}
+                                                                </span>
+                                                            </div>
+                                                        </div>
+                                                    )}
+
                                                 </div>
                                             </div>
                                             <div className="w-full h-full">
